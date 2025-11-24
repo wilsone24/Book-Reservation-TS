ba689977b44819854b9c4bfc6483bf5f88f50b80
wilsone24 <wilsone@uninorte.edu.co>
2025-11-24 11:21:50 -0500

    style: format code for consistency in db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,14 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 dotenv.config();
 
 export default function handleMongoConnection() {
-    mongoose.connect(process.env.MONGO_CONN_STRING as string).then(() => {
-        console.log("Connected to mongo server.");
-    }).catch((err) => {
-        console.error("Mongo connection error:", err);
+  mongoose
+    .connect(process.env.MONGO_CONN_STRING as string)
+    .then(() => {
+      console.log("Connected to mongo server.");
+    })
+    .catch((err) => {
+      console.error("Mongo connection error:", err);
     });
 }
