c1066831f38ff599810715f97bdd55beb25263a4
wilsone24 <wilsone@uninorte.edu.co>
2025-11-24 16:56:33 -0500

    fix: update user routes path to include server version in app initialization

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ export default function createApp() {
   const app = express();
   app.use(cors());
   app.use(express.json());
-  app.use("/users", userRoutes);
+  app.use(SERVER_VERSION + "users", userRoutes);
   app.use(routeNotFound);
   return app;
 }
