076159223d52646fffd3b83b756d1e7daec3967f
wilsone24 <wilsone@uninorte.edu.co>
2025-11-26 22:07:29 -0500

    refactor: restructure JWT verification and enhance user permission checks

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,23 +1,52 @@
-import jwt from "jsonwebtoken";
 import { NextFunction, Request, Response } from "express";
 import { HeaderUserType } from "../user/user.types";
+import checkEnabledUserAction from "../user/checkenabled.user.action";
+import jwt from "jsonwebtoken";
 
-function verifyToken(request: Request, response: Response, next: NextFunction) {
+function decodeJwtValues(request: Request): HeaderUserType | null {
   const authHeader = request.headers.authorization;
+
   if (!authHeader?.startsWith("Bearer ")) {
-    return response
-      .status(401)
-      .json({ message: "Missing Authorization header" });
+    return null;
   }
+
   const token = authHeader.split(" ")[1];
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
-    (request as any).user = decoded;
-    next();
+    const jwtValues = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string
+    ) as HeaderUserType;
+
+    return jwtValues;
   } catch {
-    return response.status(401).json({ message: "Invalid token" });
+    return null;
   }
 }
 
-export { verifyToken };
+function checkUserPermission(permission: string) {
+  return async (request: Request, response: Response, next: NextFunction) => {
+    const user = decodeJwtValues(request);
+
+    if (!user) {
+      return response.status(401).json({ message: "Not authorized." });
+    }
+
+    if (!(await checkEnabledUserAction(user._id))) {
+      return response.status(401).json({ message: "User disabled." });
+    }
+
+    const targetUserId = request.params.userId;
+
+    const hasPermission =
+      user.permissions[permission] ||
+      (targetUserId && user._id === targetUserId);
+
+    if (!hasPermission) {
+      return response.status(403).json({ message: "Permission denied." });
+    }
+    next();
+  };
+}
+
+export const UserModAuthMiddleware = checkUserPermission("UPDATE-USERS");
